a3768152219d776250e24f71e011c15a07dc7f93
Thomas Hirter <t.hirter@outlook.com>
2016-09-03 21:03:41 +0200

    Korrekturen
    Schleife der chats war überflüssig.

--- pokeHook.php
+++ pokeHook.php
@@ -53,53 +53,44 @@ if($typ == "pokemon"){
 $db->bind("disappear_time", time());
 $result   =  $db->query("SELECT * FROM pokemon WHERE disappear_time > :disappear_time");
 
-print_r($result);
-die();
 foreach($result as $notifier){
 
-    $chats = $cChat->all();
+
     /**
-     * Registrierte Chats durchschleiffen
+     * Prüfen welcher chat notifications zum pokemon erhalten möchte
      */
-    foreach($chats as $chat){
+    $cNotifylist->pokemon_id    = $notifier['pokemon_id'];
+    $notifylist = $cNotifylist->Search();
+    foreach($notifylist as $notify){
         /**
-         * Prüfen ob der Aktualle Chat dieses Pokemon als Benachrichtigung festgelegt hat
+         * Prüfen ob bereits eine Benachrichtigung zu der Chat ID und der Pokemon DB ID geschickt wurde
          */
 
-        $cNotifylist->chat_id       = $chat['chat_id'];
-        $cNotifylist->pokemon_id    = $notifier['pokemon_id'];
-        if(!empty($cNotifylist->search())) {
+        $cNotified->chat_id     = $notify['chat_id'];
+        $cNotified->pokemon_id  = $notifier['id'];
+        if(empty($cNotified->search()))
+        {
             /**
-             * Prüfen ob bereits eine Benachrichtigung zu der Chat ID und der Pokemon DB ID geschickt wurde
+             * In der Datenbank eintragen das zu diesem Pokemon bereits eine Benachrichtigung an die
+             * Chat ID geschickt wurde
              */
-
-
-            $cNotified->chat_id     = $chat['chat_id'];
+            $cNotified->chat_id     = $notify['chat_id'];
             $cNotified->pokemon_id  = $notifier['id'];
-            if(empty($cNotified->search()))
-            {
-                /**
-                 * In der Datenbank eintragen das zu diesem Pokemon bereits eine Benachrichtigung an die
-                 * Chat ID geschickt wurde
-                 */
-                $cNotified->chat_id     = $chat['chat_id'];
-                $cNotified->pokemon_id  = $notifier['id'];
-                $create = $cNotified->Create();
+            $create = $cNotified->Create();
 
 
-                /**
-                 * Nachricht an telegram senden
-                 */
-                $bild = array('chat_id' => $chat['chat_id'], 'sticker' => $pokemon->getSticker($notifier['pokemon_id']));
-                $name = array('chat_id' => $chat['chat_id'], 'text' => "I hanes " . $pokemon->getName($notifier['pokemon_id']) . " gse, \nblibt no bis " . date("H:i:s", $notifier['disappear_time']));
-                $location = array('chat_id' => $chat['chat_id'], 'latitude' => $notifier['geo_lat'], 'longitude' => $notifier['geo_lng']);
+            /**
+             * Nachricht an telegram senden
+             */
+            $bild = array('chat_id' => $notify['chat_id'], 'sticker' => $pokemon->getSticker($notifier['pokemon_id']));
+            $name = array('chat_id' => $notify['chat_id'], 'text' => "I hanes " . $pokemon->getName($notifier['pokemon_id']) . " gse, \nblibt no bis " . date("H:i:s", $notifier['disappear_time']));
+            $location = array('chat_id' => $notify['chat_id'], 'latitude' => $notifier['geo_lat'], 'longitude' => $notifier['geo_lng']);
 
-                $telegram->sendSticker($bild);
-                $telegram->sendMessage($name);
-                $telegram->sendLocation($location);
-            }
-
+            $telegram->sendSticker($bild);
+            $telegram->sendMessage($name);
+            $telegram->sendLocation($location);
         }
+
     }
 
 }
