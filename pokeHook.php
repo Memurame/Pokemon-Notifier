05c756a9faed93c5c7c621a8a27b4fa7e73f17e3
Thomas Hirter <t.hirter@outlook.com>
2016-09-10 12:20:53 +0200

    test

--- pokeHook.php
+++ pokeHook.php
@@ -95,7 +95,7 @@ if($typ == "pokemon"){
         $notifylist = $cNotifylist->Search();
         foreach($notifylist as $notify){
             $cChat->find($notify['chat_id']);
-            echo $cChat->place." -> ". $place[0];
+            echo $cChat->place." -> ". $place[0]."<br>";
             if($cChat->place == $place[0]){
                 /**
                  * Prüfen ob bereits eine Benachrichtigung zu der Chat ID und der Pokemon DB ID geschickt wurde
