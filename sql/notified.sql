33ce765dbe2c903c3cda6e187aadf90d147477b7
Thomas Hirter <t.hirter@outlook.com>
2016-09-11 21:58:17 +0200

    Delete SQL & Change comment

--- sql/notified.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS `notified` (
-  `chat_id` varchar(30) DEFAULT NULL,
-  `pokemon_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/notifylist.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `notifylist` (
-  `chat_id` varchar(30) NOT NULL,
-  `pokemon_id` int(11) NOT NULL,
-  PRIMARY KEY (`chat_id`,`pokemon_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
