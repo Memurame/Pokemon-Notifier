0b7a55861d3c45eb889bbb7a367b0ff9fe44b4f6
Thomas Hirter <t.hirter@outlook.com>
2016-09-19 03:14:11 +0200

    prepare for sqlite support

--- install/database.sql
+++ install/database.sql
@@ -15,17 +15,4 @@ CREATE TABLE `notify_pokemon` (
   `chat_id` varchar(30) CHARACTER SET utf8 NOT NULL,
   `pokemon_id` int(11) NOT NULL,
   PRIMARY KEY (`chat_id`,`pokemon_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `pokemon` (
-  `pokemon_id` int(11) DEFAULT NULL,
-  `encounter_id` varchar(250) DEFAULT NULL,
-  `disappear_time` varchar(100) DEFAULT NULL,
-  `geo_lat` float DEFAULT NULL,
-  `geo_lng` float DEFAULT NULL,
-  `spawnpoint_id` varchar(50) DEFAULT NULL,
-  `place` varchar(45) DEFAULT NULL,
-  `iv_attack` int(11) DEFAULT NULL,
-  `iv_defense` int(11) DEFAULT NULL,
-  `iv_stamina` int(11) DEFAULT NULL,
-  `iv_result` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
