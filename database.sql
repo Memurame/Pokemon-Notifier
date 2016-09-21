818ef6c5297504c0d04120a2db44bdafd9830aef
Thomas Hirter <t.hirter@outlook.com>
2016-09-21 21:04:10 +0200

    Update

--- database.sql
+++ database.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS `chats` (
-  `chat_id` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `chat_id` varchar(50) NOT NULL,
   `admin` int(11) DEFAULT '0',
   `place` varchar(45) DEFAULT NULL,
   `priority` int(11) DEFAULT NULL,
+  `alias` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`chat_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `notify_iv` (
