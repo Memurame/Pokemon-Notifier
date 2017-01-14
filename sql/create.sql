23ce1588eb6a850cf2238d623e707d6b4f9b1e24
Tom Hirter <t.hirter@outlook.com>
2017-01-14 14:54:13 +0100

    Add pokemonhistory Table to sql file

--- sql/create.sql
+++ sql/create.sql
@@ -17,4 +17,10 @@ CREATE TABLE IF NOT EXISTS `notify_pokemon` (
   `chat_id` varchar(30) CHARACTER SET utf8 NOT NULL,
   `pokemon_id` int(11) NOT NULL,
   PRIMARY KEY (`chat_id`,`pokemon_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `pokemonhistory` (
+  `encounter_id` varchar(200) NOT NULL,
+  `pokemon_id` int(4) NOT NULL,
+  `spawnpoint_id` varchar(50) NOT NULL,
+  `disappear_time` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
